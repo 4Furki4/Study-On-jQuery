62e99df765a0a25b67cd0cff15fd0126d5acc748
Furkan <84590614+4Furki4@users.noreply.github.com>
2022-10-25 23:02:57 +0300

    starting to code scripts

--- AddingFormInputs/index.js
+++ AddingFormInputs/index.js
@@ -1,5 +1,15 @@
 $("document").ready(function(){
 
-    $("#p1").text("Guess it works...");
+    $("#first-part-btn").on("click", function(){
+        console.log("first-part-btn clicked")
+    })
+
+
 });
+//
+//    <input type="text" class="form-control mt-1" id="word0-def0">
+//    $("#first-part >  #first-part-def").append('<input type="text" class="form-control mt-1" id="word0-def0">')
+//    $("#first-part >  #first-part-type").append('<input type="text" class="form-control mt-1" id="word0-def0">')
+//
+
 
