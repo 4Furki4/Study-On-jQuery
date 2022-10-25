aca30b2ee12f301dcaba3961adb5e69588addb6d
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-10-26 01:02:05 +0300

    simulation is complete for now. Adding input works well.

--- AddingFormInputs/index.js
+++ AddingFormInputs/index.js
@@ -1,15 +1,36 @@
-$("document").ready(function(){
 
-    $("#first-part-btn").on("click", function(){
-        console.log("first-part-btn clicked")
-    })
+$("document").ready(function () {
 
-
+    let firstBtnCount = 1;
+    let secondBtnCount = 1;
+    let thirdBtnCount = 1;
+    let fourthBtnCount = 1;
+    $("#first-part-btn").on("click", function () {
+        addInputs(firstBtnCount, "first-part", 0);
+        firstBtnCount++;
+    });
+    $("#second-part-btn").on("click", function () {
+        addInputs(secondBtnCount, "second-part", 1);
+        secondBtnCount++;
+    });
+    $("#third-part-btn").on("click", function () {
+        addInputs(thirdBtnCount, "third-part", 2);
+        thirdBtnCount++;
+    });
+    $("#fourth-part-btn").on("click", function () {
+        addInputs(fourthBtnCount, "fourth-part", 3);
+        fourthBtnCount++;
+    });
+    function addInputs(counter, part, wordOrder) {
+        var firstPartDef = `#${part}>#${part}-def`
+        let firstPartType = `#${part}>#${part}-type`
+        let defId = `word${wordOrder}-def${counter}`
+        let typeId = `word${wordOrder}-type${counter}`
+        var appendDef = `<input type="text" class="form-control mt-1" id="${defId}"/>`
+        let appendType = `<input type="text" class="form-control mt-1" id="${typeId}"/>`
+        $(firstPartDef).append(appendDef)
+        $(firstPartType).append(appendType)
+        $("#" + defId).hide().show("fast");
+        $("#" + typeId).hide().show("fast");
+    }
 });
-//
-//    <input type="text" class="form-control mt-1" id="word0-def0">
-//    $("#first-part >  #first-part-def").append('<input type="text" class="form-control mt-1" id="word0-def0">')
-//    $("#first-part >  #first-part-type").append('<input type="text" class="form-control mt-1" id="word0-def0">')
-//
-
-
