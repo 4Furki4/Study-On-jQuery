829051f9d972126c35c2cb0f2c873b2dcc06c00a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-10-26 20:55:06 +0300

    grid system is changed, rows will be added eached button click to add new input line. I also added delete buttons by type input but not yet functional

--- AddingFormInputs/index.js
+++ AddingFormInputs/index.js
@@ -1,6 +1,13 @@
 
 $("document").ready(function () {
 
+        $("#first-part").hide().slideDown("slow");
+        $("#second-part").hide().slideDown("slow");
+        $("#third-part").hide().slideDown("slow");
+        $("#fourth-part").hide().slideDown("slow");
+
+
+
     let firstBtnCount = 1;
     let secondBtnCount = 1;
     let thirdBtnCount = 1;
@@ -22,15 +29,26 @@ $("document").ready(function () {
         fourthBtnCount++;
     });
     function addInputs(counter, part, wordOrder) {
-        var firstPartDef = `#${part}>#${part}-def`
-        let firstPartType = `#${part}>#${part}-type`
+        let delBtnOrder = `${part}-del-btn-${counter}`
         let defId = `word${wordOrder}-def${counter}`
         let typeId = `word${wordOrder}-type${counter}`
-        var appendDef = `<input type="text" class="form-control mt-1" id="${defId}"/>`
-        let appendType = `<input type="text" class="form-control mt-1" id="${typeId}"/>`
-        $(firstPartDef).append(appendDef)
-        $(firstPartType).append(appendType)
-        $("#" + defId).hide().show("fast");
-        $("#" + typeId).hide().show("fast");
+        let appendPart = `#${part}`
+        let row = `
+        <div class="row">
+            <div class="offset-1 col-2">
+                <span class="bg-secondary float-end mt-2"><h3>${counter+1}.</h3></span>
+            </div>
+            <div class="col-4" id="first-part-def">
+                <input type="text" class="form-control mt-1" id="word${wordOrder}-def${counter}">
+            </div>
+            <div class="col-3" id="first-part-type">
+                <input type="text" class="form-control mt-1" id="word${wordOrder}-type${counter}">
+            </div>
+            <input id="${delBtnOrder}" class="btn btn-dark mt-1" type="button" value="X" style="width: 40px; height: 40px;">
+        </div>`
+        $(appendPart).append(row);
+        $("#"+defId).hide().show("slow");
+        $("#"+typeId).hide().show("slow");
+        $(`#${delBtnOrder}`).hide().slideDown("slow")
     }
 });
